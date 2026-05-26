f1897fd2ea6beab2200dcef8e18bf8a3d86a3f80
Kawan.VPeniche <kawanvinicius742@gmail.com>
2026-05-25 22:11:11 -0300

    attAula10

---
+++ aula-10-introducao-ao-javascript/02-operadores/script.js
@@ -0,0 +1,46 @@
+// JAVASCRIPT
+
+let n1 = Number(window.prompt("Digite um número: "));
+let n2 = Number(window.prompt("Digite outro número: "));
+
+// SOMA
+
+const soma = n1 + n2;
+
+window.alert(`A soma dos valores é ${soma}`);
+
+// DEMAIS OPERADORES
+
+// MULTIPLICAÇÃO
+
+let mult = n1 * n2;
+document.writeln(`<br>A multiplicação de ${n1} e ${n2} é igual a ${mult}.`);
+
+// SUBTRAÇÃO
+
+let sub = n1 - n2;
+document.writeln(`<br>A subtração de ${n1} e ${n2} é igual a ${sub}.`);
+
+// POTÊNCIA
+
+let pot = n1 ** n2;
+document.writeln(`<br>A potência de ${n1} e ${n2} é igual a ${pot}.`);
+
+// DIVISÃO REAL
+
+let divr = n1 / n2;
+document.writeln(`<br>A divisão real de ${n1} e ${n2} é igual a ${divr}.`);
+
+// RESTO DA DIVISÃO
+
+let rdiv = n1 % n2;
+document.writeln(`<br>O resto da divisão de ${n1} e ${n2} é igual a ${rdiv}.`);
+
+// OPERADOR DE ATRIBUIÇÃO
+
+n1 += n2;
+document.writeln(`<br><br> ${n1}`);
+n1 += n2;
+document.writeln(`<br><br> ${n1}`);
+n1 += n2;
+document.writeln(`<br><br> ${n1}`);
